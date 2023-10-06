c587f2d44e2f5b500b187cf901eb0ae22e0084cb
shubham007x <shubham.agdari@gmail.com>
2023-10-06 15:08:55 +0530

    portfolio

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -41,19 +41,12 @@ const Home = () => {
             <br />I plan to learn <b>Next.js</b> in the near future. <br />
             <br />
           </p>
-          <a
-            id="resume-link-2"
-            onClick={() => {
-              navigate("/resume");
-            }}
-            download="Shubham Agdari Resume pdf"
-            href={pdf}
-          >
+          <a id="resume-link-2" href={pdf} download="Shubham Agdari Resume">
             Resume <FaDownload />
           </a>
         </div>
         <Tilt>
-          <img className="Avatar home-img" src={Avatar} alt="" />
+          <img className="Avatar" src={Avatar} alt="" />
         </Tilt>
       </div>
     </section>
